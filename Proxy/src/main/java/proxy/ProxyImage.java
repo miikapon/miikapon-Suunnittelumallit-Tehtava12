8f89af2b323e6058da2ef78f747d0f45a0a1c17c
miikapon <33020766+miikapon@users.noreply.github.com>
2020-11-18 10:01:30 +0200

    first commit
    first commit

---
+++ Proxy/src/main/java/proxy/ProxyImage.java
@@ -0,0 +1,25 @@
+package proxy;
+
+public class ProxyImage implements Image {
+
+    private final String filename;
+    private RealImage image;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void displayImage() {
+        if (image == null) {
+            image = new RealImage(filename);
+        }
+        image.displayImage();
+    }
+
+    @Override
+    public void showData() {
+        System.out.println("Image name: " + filename);
+    }
+
+}
